ecc3b33511c71e16ff6069de5e0153739cb23f59
Kevin Thornbloom <kevin@btprosolutions.com>
2025-07-17 17:36:02 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/Proto-Heavytech-AI-Vision/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Heavytech-Dashboard/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
